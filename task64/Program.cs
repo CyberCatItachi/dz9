849874339de57dae311441b922b5287341691373
Viktoria <michinyan1404@gmail.com>
2023-07-20 18:51:57 +0300

    Tasks

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Bведите N: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine(PrintNums(n));
+
+string PrintNums(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    string s = n.ToString() + ", " + PrintNums(n-1);
+    return s;
+}
---
+++ task66/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите М: ");
+int m = int.Parse(ReadLine()!);
+
+WriteLine("Введите N: ");
+int n = int.Parse(ReadLine()!);
+
+WriteLine(PrintNumbers(m, n));
+
+int PrintNumbers(int m, int n)
+{
+    if (n == m || n == 1)
+    {
+        return n;
+    }
+    int s = PrintNumbers(m, n - 1)+n;
+    return s;
+}
